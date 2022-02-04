1d1510b75b1d724002963c1b4f2f5adbc10ee255
ianakt <68198233+ianakt@users.noreply.github.com>
2022-02-03 21:06:12 -0500

    adding unique titles and updated database challenge sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -5,3 +5,11 @@ RIGHT JOIN titles as ti
 ON (em.emp_no = ti.emp_no)
 WHERE (em.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY(em.emp_no)
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no, rt.first_name, rt.last_name, rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE rt.to_date = ('9999-01-01')
+ORDER BY  rt.emp_no ASC, rt.to_date DESC
